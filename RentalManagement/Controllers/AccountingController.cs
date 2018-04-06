8f2f5c797c0bb24ea3f7f1241a54db3e0519b271
CodeMyJava <Applenourishment@gmail.com>
2018-04-06 15:13:29 -0700

    Revert "Linked data model to accounting page |   merge conflict resolved hopefully"

--- RentalManagement/Controllers/AccountingController.cs
+++ RentalManagement/Controllers/AccountingController.cs
@@ -3,35 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using RentalManagement.Models;
 
 namespace RentalManagement.Controllers
 {
     [Authorize(Roles = "Admin, Manager, Staff")]
     public class AccountingController : Controller
     {
-
-        private ApplicationDbContext accountContext;
-
-        public AccountingController()
-        {
-            accountContext = new ApplicationDbContext();
-        }
-
         // GET: Accounting
-        // Displays the first index page
         public ActionResult Index()
         {
             ViewBag.Message = "Accounting page.";
 
-           var aList = accountContext.Occupancies.ToList();
-            //var myList = accountContext.Occupancies.ToList;
-            // Where clause with lambda
-
-            return View(aList);
+            return View();
         }
         // GET: Accounting//PaymentDetails
-        // Displays the second page, payment details
         public ActionResult PaymentDetails()
         {
             ViewBag.Message = "Payment Details page.";
@@ -39,14 +24,11 @@ namespace RentalManagement.Controllers
             return View();
         }
         // GET: Accounting/OrderDetails()
-        // Display the final page for confirmation
         public ActionResult OrderDetails()
         {
             ViewBag.Message = "Order Details page.";
 
-            var aList = accountContext.Occupancies.ToList();
-
-            return View(aList);
+            return View();
         }
     }
 }
