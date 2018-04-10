119147a7375723b5f33f28a21d0cf4999b52b859
Michael Navarro <navarrom0412@gmail.com>
2018-04-10 11:48:02 -0700

    Role-based auth

--- RentalManagement/Controllers/AssetsController.cs
+++ RentalManagement/Controllers/AssetsController.cs
@@ -6,10 +6,12 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using RentalManagement.CustomFilters;
 using RentalManagement.Models;
 
 namespace RentalManagement.Controllers
 {
+    [AuthLog(Roles = "Tenant")]
     public class AssetsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
--- RentalManagement/Controllers/HomeController.cs
+++ RentalManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentalManagement.CustomFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace RentalManagement.Controllers
 {
+    [AuthLog(Roles = "Admin, Staff")]
     public class HomeController : Controller
     {
         public ActionResult Index()
