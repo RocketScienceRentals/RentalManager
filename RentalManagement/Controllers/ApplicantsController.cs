ed9b309dc708141d4b39d76e4ac1d8ea8a8fce40
Michael Navarro <navarrom0412@gmail.com>
2018-04-13 06:04:33 -0700

    App updates AspNetUsers

--- RentalManagement/Controllers/ApplicantsController.cs
+++ RentalManagement/Controllers/ApplicantsController.cs
@@ -156,41 +156,47 @@ namespace RentalManagement.Controllers
             using (var db = new ApplicationDbContext())
             {
                 Applicant applicant = db.Applicants.Find(id);
-
-                /*
-                 * UserName must be Email
-                 */
-                var user = new ApplicationUser { UserName = applicant.Email, Email = applicant.Email };
-                string password = "Password1!";
-
-                var store = new UserStore<ApplicationUser>(db);
-                var manager = new UserManager<ApplicationUser, string>(store);
-
-                var result = manager.Create(user, password);
-                if (!result.Succeeded)
-                    throw new ApplicationException("Unable to create a user.");
-
-                result = manager.AddToRole(user.Id, "Tenant");
-                if (!result.Succeeded)
-                    throw new ApplicationException("Unable to add user to a role.");
-
                 Asset asset = db.Assets.Find(applicant.AssetID);
 
-                Tenant tenant = new Tenant { ID = Guid.NewGuid(),
-                                             Name = applicant.Name,
-                                             Email = applicant.Email,
-                                             Details = applicant.Details,
-                                             RequestedAssets = asset
-                                           };
+                Tenant tenant = new Tenant
+                {
+                    ID = Guid.NewGuid(),
+                    Name = applicant.Name,
+                    Email = applicant.Email,
+                    Details = applicant.Details,
+                    RequestedAssets = asset
+                };
                 db.Tenants.Add(tenant);
+                //db.Applicants.Remove(applicant);
                 db.SaveChanges();
 
-                //var client = new SmtpClient("smtp.gmail.com", 587)
-                //{
-                //    Credentials = new NetworkCredential("myusername@gmail.com", "mypwd"),
-                //    EnableSsl = true
-                //};
-                //client.Send("myusername@gmail.com", "myusername@gmail.com", "Your Tenant Account", "Your password is: " + password);
+                Tenant tenant2 = db.Tenants.Find(tenant.ID);
+                if (tenant2 != null)
+                {
+                    // UserName == Email
+                    var user = new ApplicationUser { UserName = applicant.Email, Email = applicant.Email };
+                    string password = "Password1!";
+
+                    user.Tenant = tenant2;
+
+                    var store = new UserStore<ApplicationUser>(db);
+                    var manager = new UserManager<ApplicationUser, string>(store);
+
+                    var result = manager.Create(user, password);
+                    if (!result.Succeeded)
+                        throw new ApplicationException("Unable to create a user.");
+
+                    result = manager.AddToRole(user.Id, "Tenant");
+                    if (!result.Succeeded)
+                        throw new ApplicationException("Unable to add user to a role.");
+
+                    //var client = new SmtpClient("smtp.gmail.com", 587)
+                    //{
+                    //    Credentials = new NetworkCredential("myusername@gmail.com", "mypwd"),
+                    //    EnableSsl = true
+                    //};
+                    //client.Send("myusername@gmail.com", "myusername@gmail.com", "Your Tenant Account", "Your password is: " + password);
+                }
             }
         }
     }
